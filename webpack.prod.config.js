2e645b9d740f00d191af3c0048f6c00334718ba9
Yasmine Triola <yasmine@yasminetriola.com>
2019-12-10 15:23:59 -0500

    Remove uglifyjs-webpack-plugin (deprecated) and replace with terser-webpack-plugin.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 
 module.exports = {
@@ -55,7 +55,7 @@ module.exports = {
     ]
   },
   optimization: {
-      minimizer: [new UglifyJsPlugin()],
+      minimizer: [new TerserPlugin()],
   },
   plugins: [
       new MiniCssExtractPlugin({
