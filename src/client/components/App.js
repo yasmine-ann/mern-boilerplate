7e657a51caab0e00d5821fbeafc6d7d7edf18ec3
Yasmine Triola <yasmine@yasminetriola.com>
2020-01-02 15:47:58 -0500

    Add basic routing with react-router-dom.

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
 function App() {
     return (
-        <div className="welcome">
-            <h1>Yaz&apos;s MERN Boilerplate</h1>
-            <p>This is a MERN Boilerplate project using Webpack</p>
+        <Router>
+            <div className="welcome">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <h1>Yaz&apos;s MERN Boilerplate</h1>
+
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+
+            </div>
+
+        </Router>
+    )
+}
+
+function Home() {
+    return (
+        <div>
+            <p>This is a MERN Boilerplate project.</p>
+        </div>
+    )
+}
+
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>This is a MERN Boilerplate project featuring Webpack 4.0.</p>
         </div>
     )
 }
