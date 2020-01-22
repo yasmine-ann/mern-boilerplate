1f3eb5582d397a1089277dfba3a0354ad7463382
Yasmine Triola <yasmine@yasminetriola.com>
2020-01-22 17:26:18 -0500

    Fix production build fallback url response path.

--- src/server/server.js
+++ src/server/server.js
@@ -7,15 +7,16 @@ import mongoose from 'mongoose';
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
-const isDevelopment = process.env.NODE_ENV === "development" ? true : false;
+const history = require('connect-history-api-fallback');
 
 const app = express();
 
-//Enable cors
-app.use(cors())
+//Enable cors. This is the most basic implementation, please see cors documentation
+//for further customization options.
+app.use(cors());
 
 //DB setup
-mongoose.Promise = Promise;
+mongoose.Promise = global.Promise;
 mongoose.connect(MONGODB_URI);
 var db = mongoose.connection;
 
@@ -24,7 +25,10 @@ db.once('open', function() {
     console.log('We have been connected!');
 });
 
-if (isDevelopment) {
+
+//If NODE_ENV is set to development in .env, execute development environment settings
+if (process.env.NODE_ENV !== "production") {
+    app.use(history());
     const webpack = require('webpack');
     const webpackDevMiddleware = require('webpack-dev-middleware');
     //require webpack config
@@ -37,9 +41,16 @@ if (isDevelopment) {
     }));
     //enable HMR
     app.use(require('webpack-hot-middleware')(compiler));
+    app.use(express.static("./dist"));
 } else {
-    app.use(express.static(path.resolve(__dirname, 'dist')));
+    app.use(express.static("./dist"));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve('./dist', 'index.html'));
+    });
 }
+
+
 
 //Listen
 app.listen(PORT, function() {
