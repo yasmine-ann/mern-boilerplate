8159265677df559ec784fa2b3ea8d1302c9e2ce6
Yasmine Triola <yasmine@yasminetriola.com>
2020-01-28 14:12:59 -0500

    Add pages folder and move App.js into it.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/client/components/App.js';
+import App from './client/pages/App.js';
 
 import './client/assets/scss/main.scss';
 
